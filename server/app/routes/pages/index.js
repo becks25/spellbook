d6aa4082d47211304c3d69822723ac6c8caf98f8
Austin <tashoecraft@gmail.com>
2015-10-01 19:02:17 -0400

    added security to crud routes

--- server/app/routes/pages/index.js
+++ server/app/routes/pages/index.js
@@ -15,37 +15,45 @@ router.get('/:pageId', (req, res, next) => {
 });
 
 //create one
+// passes in the body
 router.post('/:storyId', (req, res, next) => {
-    Page.create(req.body.newPage)
-        .then(page => {
-            req.story.pages.push(page._id);
-            req.story.save()
-                .then(page => res.status(201).send(page))
-        })
-        .then(null, next);
+    if(req.user) {
+        Page.create(req.body.newPage)
+            .then(page => {
+                req.story.pages.push(page._id);
+                req.story.save()
+                    .then(page => res.status(201).send(page))
+            })
+            .then(null, next);
+    } next();
 });
 
 //edit one
+//pass the author and the edits in the body
 router.put('/:pageId', (req, res, next) => {
-    _.assign(req.page, req.body.editPage);
-    req.page.save()
-        .then(page => res.status(200).send(page))
-        .then(null, next);
+    if(req.user === req.body.author || req.user.isAdmin) {
+        _.assign(req.page, req.body.editPage);
+        req.page.save()
+            .then(page => res.status(200).send(page))
+            .then(null, next);
+    } next();
 });
 
 //delete one
 router.delete('/:pageId', (req, res, next) => {
-    Page.remove({_id: req.page._id}).exec()
-        .then(removed => {
-            Story.find()
-                .where({pages: {$in: req.page._id}})
-                .then(story => {
-                    _.remove(story.pages, n => n === req.page._id);
-                    story.save()
-                        .then(removed => res.status(200).send(removed))
-                })
-        })
-        .then(null, next);
+    if(req.user === req.body.author || req.user.isAdmin) {
+        Page.remove({_id: req.page._id}).exec()
+            .then(removed => {
+                Story.find()
+                    .where({pages: {$in: req.page._id}})
+                    .then(story => {
+                        _.remove(story.pages, n => n === req.page._id);
+                        story.save()
+                            .then(removed => res.status(200).send(removed))
+                    })
+            })
+            .then(null, next);
+    } next();
 });
 
 router.param('pageId', (req, res, next, pageId) => {
