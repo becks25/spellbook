8f0095240a8f1ee726236d31e5b968b47d4f5d22
Chandra Galbiati <chandra.galbiati@gmail.com>
2015-10-01 21:57:29 -0400

    added to seed

--- seed.js
+++ seed.js
@@ -22,13 +22,53 @@ var Promise = require('bluebird');
 var chalk = require('chalk');
 var connectToDb = require('./server/db');
 var User = Promise.promisifyAll(mongoose.model('User'));
+var Story = Promise.promisifyAll(mongoose.model('Story'));
+var Page = Promise.promisifyAll(mongoose.model('Page'));
 
-var seedUsers = function () {
+
+
+var concepts = ['loops', 'if-statements', 'functions', 'recursion', 'asynch']
+var descr = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad.";
+
+var seedStories = function(){
+    var titles = ['Happy Dance', 'Omri', 'Joe\'s Salt', 'Adventures of Mark'];
+
+    var stories = titles.map(function(title){
+        return {
+            title: title,
+            description: descr,
+            difficulty: Math.floor(Math.random()*10),
+            concepts: [concepts[Math.floor(Math.random()*concepts.length)]]
+        };
+    });
+     return Story.createAsync(stories);
+};
+
+var seedPages = function(stories){
+    var pages = [];
+    stories.forEach(function(story){
+        for (i=0; i<3; i++){
+            pages.push({
+                story: story._id,
+                text: descr,
+                tools: ['move', 'pick-up', 'put-down', 'ask', 'tell', 'if', 'loop'],
+                variables: ['Omri', 'Zeke', 'Joe', 'Say hi'],
+                concepts: [concepts[Math.floor(Math.random()*concepts.length)]],
+                hint: 'Try harder',
+                gameboard: [],
+                requirements: []
+            });
+        }
+    });
+     return Page.createAsync(pages);
+};
+
+var seedUsers = function (stories, pages) {
 
     var users = [
         {
             email: 'testing@fsa.com',
-            password: 'password'
+            password: 'password',
         },
         {
             email: 'obama@gmail.com',
@@ -36,18 +76,48 @@ var seedUsers = function () {
         }
     ];
 
+    var makeAge = function(min, max){
+        return Math.floor(Math.random()*(max-min)) + min;
+    };
+
+    users.forEach(function(user){
+        user.username = user.email;
+        user.age = makeAge(5, 13);
+        user.gender = Math.random() > 0.3 ? 'female' : 'male';
+        user.character = {name: 'Omri'};
+        user.mastery = [];
+        concepts.forEach(function(concept){
+            user.mastery.push({
+                topic: concept,
+                pointsEarned: Math.floor(Math.random()* 100),
+                pointsPossible: Math.floor(Math.random()* 100) + 100
+            });
+        completedStories = [stories[Math.floor(Math.random()*stories.length)]._id];
+        unfinishedPages = [pages[Math.floor(Math.random()*pages.length)]._id];
+        });
+
+    });
+
     return User.createAsync(users);
 
 };
 
 connectToDb.then(function () {
-    User.findAsync({}).then(function (users) {
-        if (users.length === 0) {
-            return seedUsers();
-        } else {
-            console.log(chalk.magenta('Seems to already be user data, exiting!'));
-            process.kill(0);
-        }
+    // User.findAsync({}).then(function (users) {
+        // if (users.length === 0) {
+        //     return seedUsers();
+        // } else {
+        //     console.log(chalk.magenta('Seems to already be user data, exiting!'));
+        //     process.kill(0);
+        // }
+    var tempUsers, tempStories, tempPages;
+    seedStories()
+    .then(function(stories){
+        tempStories = stories;
+        return seedPages(tempStories);
+    }).then(function(pages){
+        tempPages = pages;
+        return seedUsers(tempStories, tempPages);
     }).then(function () {
         console.log(chalk.green('Seed successful!'));
         process.kill(0);
