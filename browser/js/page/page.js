f2e98e61a91dba727b78e44bff1eeb0cdd921e54
Austin <tashoecraft@gmail.com>
2015-10-08 21:10:48 -0400

    save

--- browser/js/page/page.js
+++ browser/js/page/page.js
@@ -45,12 +45,11 @@ app.controller('PageCtrl', ($scope, AuthService, $state, page, ClassFactory, SPR
     // takes vars and tools from page model and makes command objs
     // pushes each obj to spellTools arr
     var spellToolConstr = () => {
-        console.log('&&&&&& page tools', $scope.page.tools)
         $scope.page.tools.forEach((tool)=> {
             var newTool = SpellComponentFactory.makeToolObj(tool);
             $scope.spellTools.push(newTool);
         });
-    }
+    };
     //construct the spellTools arr on load
     spellToolConstr();
 
