b3bcb8019dbc8b8e44b60cc8c974e1e9063840d5
Beckylee <dr.oreo@gmail.com>
2015-10-11 17:21:00 -0400

    age and gender charts fully functional

--- browser/js/common/directives/admin-stats/admin-stats.js
+++ browser/js/common/directives/admin-stats/admin-stats.js
@@ -3,36 +3,33 @@ app.directive('adminStats', function(UserFactory) {
     restrict: 'E',
     templateUrl: '/js/common/directives/admin-stats/admin-stats.html',
     link: (scope, elem) => {
-        var w = document.querySelector('#ageChart').clientWidth;
-
-        var genders = ['male', 'female', 'other', 'prefer not to say'];
-
-        console.log(scope.genderDist);
-
 
         //Make age distribution chart
-        var ages = Array(18).join(0).split('');
+        var ages = Array(20).join(0).split('');
 
-        var objData = _.assign(ages, scope.ageDist);
+        var temp = _.assign(ages, scope.ageDist);
+
+        var objData = _.omit(temp, [0, 1, 2, 'undefined']);
 
         var w = document.querySelector('#ageChart').clientWidth;
 
-        var h = 100;
+        var h = 150;
 
         var xScale = d3.scale.ordinal()
-                        .domain(d3.range(Object.keys(objData).length))
-                        .rangeRoundBands([0, w], 0.05); 
+                        .domain(_.range(3,19))
+                        .rangeRoundBands([0, w-20], 0.05); 
 
         var yScale = d3.scale.linear()
                         .domain([0, d3.max(Object.keys(objData), function(d) {
                             return objData[d];
                         })])
-                        .range([0, h-30]);
+                        .range([0, h-60]);
 
         var xAxis = d3.svg.axis()
                       .scale(xScale)
                       .orient("bottom")
-                      .ticks(20);
+                      .tickValues(_.range(3,19))
+                      .ticks(15);
 
         var key = function(d) {
             return d;
@@ -44,9 +41,9 @@ app.directive('adminStats', function(UserFactory) {
                     .attr("width", w)
                     .attr("height", h)
                     .append("g")
-                    .attr("transform", "translate(0, -20)");
+                    .attr("transform", "translate(20, -20)");
 
-
+        //add the x axis
         svg.append("g")
             .attr("class", "x axis")
             .attr("transform", "translate(0," + h + ")")
@@ -54,8 +51,7 @@ app.directive('adminStats', function(UserFactory) {
           .selectAll("text")
             .style("text-anchor", "end")
             .attr("dx", "0.3em")
-            .attr("dy", "0.5em");
-
+            .attr("dy", "0.5em")
 
         //Create bars
         svg.selectAll("rect")
@@ -63,7 +59,7 @@ app.directive('adminStats', function(UserFactory) {
            .enter()
            .append("rect")
            .attr("x", function(d, i) {
-                return xScale(i);
+                return xScale(d);
            })
            .attr("y", function(d) {
                 return h - yScale(objData[d]);
@@ -76,85 +72,89 @@ app.directive('adminStats', function(UserFactory) {
                 return "rgb(0, 0, " + (d * 10) + ")";
            })
 
-            //Tooltip
-            .on("mouseover", function(d) {
-                //Get this bar's x/y values, then augment for the tooltip
-                var xPosition = parseFloat(d3.select(this).attr("x")) + xScale.rangeBand() / 2;
-                var yPosition = parseFloat(d3.select(this).attr("y")) + 14;
-                
-                //Update Tooltip Position & value
-                d3.select("#tooltip")
-                    .style("left", xPosition + "px")
-                    .style("top", yPosition + "px")
-                    .select("#value")
-                    .text(d);
-                d3.select("#tooltip").classed("hidden", false)
-            })
-            .on("mouseout", function() {
-                //Remove the tooltip
-                d3.select("#tooltip").classed("hidden", true);
-            });
-
         //Create labels
-        svg.selectAll("text")
-           .data(Object.keys(objData), key)
-           .enter()
-           .append("text")
-           .text(function(d) {
-                return d;
-           })
-           .attr("text-anchor", "middle")
-           .attr("x", function(d, i) {
-                return xScale(i) + xScale.rangeBand() / 2;
-           })
-           .attr("y", function(d) {
-                return h -10;
-           })
-           .attr("font-family", "sans-serif") 
-           .attr("font-size", "11px")
-           .attr("fill", "white");
-           
-        var sortOrder = false;
-        var sortBars = function () {
-            sortOrder = !sortOrder;
-            
-            sortItems = function (a, b) {
-                if (sortOrder) {
-                    return a - b;
-                }
-                return b - a;
-            };
+        Object.keys(objData).forEach(age => {
+          svg.append("text")
+             .data(age)
+             .text(function(d) {
+                  return objData[age];
+             })
+             .attr("text-anchor", "middle")
+             .attr("x", function(d, i) {
+                  return xScale(age) + xScale.rangeBand() / 2;
+             })
+             .attr("y", function(d) {
+                  return h -10;
+             })
+             .attr('fill', 'white');
+        });
 
-            svg.selectAll("rect")
-                .sort(sortItems)
-                .transition()
-                .delay(function (d, i) {
-                return i * 50;
-            })
-                .duration(1000)
-                .attr("x", function (d, i) {
-                return xScale(i);
-            });
-
-            svg.selectAll('text')
-                .sort(sortItems)
-                .transition()
-                .delay(function (d, i) {
-                return i * 50;
-            })
-                .duration(1000)
-                .text(function (d) {
-                return d;
-            })
-                .attr("text-anchor", "middle")
-                .attr("x", function (d, i) {
-                return xScale(i) + xScale.rangeBand() / 2;
-            })
-                .attr("y", function (d) {
-                return h - yScale(d) + 14;
-            });
+        //insert pie chart!
+        var genders = {
+          'male': 0, 
+          'female': 0,
+          'other': 0,
+          'prefer not to say':0
         };
 
+        var genderData = _.assign(genders, scope.genderDist);
+
+        genderData['prefer not to say'] += genderData.undefined;
+
+        var genderData = _.omit(genderData, ['undefined']);
+        var genderData =_.pairs(genderData);
+
+        var width = document.querySelector('#genderChart').clientWidth -30;
+        var r = width/2;
+        var color = d3.scale.category20c();
+
+        var data = [{"label":"Category A", "value":20}, 
+                      {"label":"Category B", "value":50}, 
+                      {"label":"Category C", "value":30}];
+
+
+        var vis = d3.select('#genderChart')
+                    .append("svg")
+                    .data([genderData])
+                    .attr("width", width)
+                    .attr("height", width)
+                    .append("svg:g")
+                    .attr("transform", "translate(" + r + "," + r + ")");
+
+        var pie = d3.layout.pie()
+                    .value(function(d){
+                      return d[1]; 
+                    });
+
+        // declare an arc generator function
+        var arc = d3.svg.arc().outerRadius(r-15);
+
+        // select paths, use arc generator to draw
+        var arcs = vis.selectAll("g.slice")
+                      .data(pie)
+                      .enter()
+                      .append("svg:g")
+                      .attr("class", "slice");
+        arcs.append("svg:path")
+            .attr("fill", function(d, i){
+                return color(i);
+            })
+            .attr("d", function (d) {
+                return arc(d);
+            });
+
+        // add the text
+        arcs.append("svg:text")
+              .attr("transform", function(d){
+                  d.innerRadius = 0;
+                  d.outerRadius = r;
+                return "translate(" + arc.centroid(d) + ")"; 
+              }).attr("text-anchor", "middle")
+              .text( function(d, i) {
+                  if(genderData[i][1] > 0)
+                  return genderData[i][0]; 
+                }
+              );
   
     }
   };
