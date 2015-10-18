bf1d7f66df7b6414268c32b125a1bed4dc63ab66
Chandra Galbiati <chandra.galbiati@gmail.com>
2015-10-17 22:53:38 -0400

    level edit adds characters and backgrounds, but both need rendering help on level edit pg - backgrounds dont actually show, and character imgs are too small for the carosel, and also sometimes dont show below and do not get removed from map

--- browser/js/common/directives/level-editor-stuff/sprite-thumbnail.js
+++
@@ -1,9 +0,0 @@
-app.directive('spriteThumbnail', function ($rootScope, $state) {
-
-    return {
-        restrict: 'E',
-        templateUrl: 'js/common/directives/level-editor-stuff/sprite-thumbnail.html',
-        link: (scope, elem, attr) => {
-        }
-    };
-  });
