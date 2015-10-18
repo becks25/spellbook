4c3e456c700566d182b644b8bebb96ee866db144
Chandra Galbiati <chandra.galbiati@gmail.com>
2015-10-18 13:11:36 -0400

    avatar and objects can be added and removed

--- browser/js/common/constants/gameboard.constant.js
+++ browser/js/common/constants/gameboard.constant.js
@@ -1,11 +1,10 @@
 app.constant('GAMEBOARD_BACKGROUNDS', [
-    'images/space.png', 
     'images/flower-field.png', 
     'images/underwater.png',
+    'images/space.png', 
     'images/mars.png', 
     'images/ocean.png', 
     'images/world.png',
-    'images/stage-bg.png', 
     'images/jungle.png', 
 
 ]);
